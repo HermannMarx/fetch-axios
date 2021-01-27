c8d2a0488315dcc07f6db405d4dc2ca39bff63e0
Hermann Marx <marxhermann@web.de>
2021-01-27 09:17:19 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import List from "./component/List";
+import withListLoading from "./component/withListLoading";
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/hacktivist123/repos`;
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
+
+  /* useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/hacktivist123/repos`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]); */
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>My Repositories</h1>
+      </div>
+      <div className="repo-container">
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className="footer">
+          Built{" "}
+          <span role="img" aria-label="love">
+            💚
+          </span>{" "}
+          with by Shedrack Akintayo
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default App;
---
+++ src/component/withListLoading.js
@@ -0,0 +1,12 @@
+function WithListLoading(Component) {
+  return function WithLoadingComponent({ isLoading, ...props }) {
+    if (!isLoading) return <Component {...props} />;
+    return (
+      <p style={{ textAlign: "center", fontSize: "30px" }}>
+        Hold on, fetching data may take some time ;)
+      </p>
+    );
+  };
+}
+
+export default WithListLoading;
